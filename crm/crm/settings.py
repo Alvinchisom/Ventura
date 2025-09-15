638ced954840ff97bb38624496a90f40c5192dc9
Alvinchisom <145863920+Alvinchisom@users.noreply.github.com>
2025-09-15 21:44:44 +0100

    well done

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
 Password = os.environ.get('PASSWORD')
+# Password = os.environ['PASSWORD']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
